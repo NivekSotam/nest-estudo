4859e0ef4982ea78fea4dc8ef6ad6840664763c4
NivekSotam <kevin.matoss.km@gmail.com>
2023-05-23 14:13:29 -0300

    livraria-nest-estudo

---
+++ src/book/book.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+
+@Controller('books')
+export class BookController {
+    constructor(private bookService: BookService){}
+
+
+    @Get()
+    async getAllBooks(): Promise<Book[]>{
+        return this.bookService.FindAll();
+    }
+
+    @Post()
+    async createBook(
+        @Body()
+        book,
+    ): Promise<Book> {
+        return this.bookService.create(book)
+    }
+}
---
+++ src/book/schemas/book.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export enum Categoria {
+    AVENTURA = 'Aventura',
+    FANTASIA = 'Fantasia',
+    FICCAO = 'Ficcao',
+    TECNOLOGIA = 'Tecnologia',
+}
+
+
+@Schema({
+    timestamps: true
+})
+
+export class Book {
+
+    @Prop()
+    nome: string;
+
+    @Prop()
+    descricao: string;
+
+    @Prop()
+    autor: string;
+
+    @Prop()
+    preco: number;
+
+    @Prop()
+    categoria: Categoria
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book)
